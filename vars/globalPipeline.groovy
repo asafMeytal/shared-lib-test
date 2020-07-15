50efcad13fb9edbc4a3861d461d59d617959a911
Asaf Meytal <asaf.meytal@automat-it.com>
2020-07-15 16:15:01 +0300

    testing 31

--- vars/globalPipeline.groovy
+++ vars/globalPipeline.groovy
@@ -2,8 +2,8 @@ def call ()
 {
 	node{
 		stage ('Test feature branch'){
-			if (env.BRANCH_NAME =~ 'feature/*'){
-				echo 'test ok'
+			if (env.BRANCH_NAME =~ 'PR*'){
+				echo 'test ok for PR worked'
 			}				
 			
 		}
