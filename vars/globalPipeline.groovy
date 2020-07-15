543a4cf5639824fb9102ec89ddd72b8530cdf558
Asaf Meytal <asaf.meytal@automat-it.com>
2020-07-15 16:43:44 +0300

    Test

--- vars/globalPipeline.groovy
+++ vars/globalPipeline.groovy
@@ -3,9 +3,15 @@ def call ()
 	node{
 		stage ('Test feature branch'){
 			if (env.BRANCH_NAME =~ 'PR*'){
+				checkout scm
 				echo 'test ok for PR worked'
-			}				
-			
+			}					
+		}
+		
+		stage ('Merge to Develop branch'){
+			if (env.BRANCH_NAME == 'develop'){
+
+			}
 		}
 	}
 }
